bb48981087cab069139755200b109e6c4e988f7f
realth000 <a598919335@gmail.com>
2022-12-11 02:17:32 +0800

    fix(*): Fix build on Linux and for Android
    Fix build on Linux and for Android.

--- include/binding.h
+++ include/binding.h
@@ -9,13 +9,11 @@ extern "C" {
 #ifdef MEIPURU_LIB
 #if (defined(_WIN32) || defined(_WIN64))
 #define MEIPURU_EXPORT __declspec(dllexport)
-#else
-#define MEIPURU_EXPORT __declspec(dllimport)
-#endif // MSVC
 #elif defined(__GNUC__)
 #define MEIPURU_EXPORT __attribute__ ((visibility("default")))
 #else
 #define MEIPURU_EXPORT
+#endif
 #endif
 
 typedef struct {
--- src/MeipuruExport.h
+++ src/MeipuruExport.h
@@ -4,13 +4,11 @@
 #ifdef MEIPURU_LIB
 #if (defined(_WIN32) || defined(_WIN64))
 #define MEIPURU_EXPORT __declspec(dllexport)
-#else
-#define MEIPURU_EXPORT __declspec(dllimport)
-#endif // MSVC
 #elif defined(__GNUC__)
 #define MEIPURU_EXPORT __attribute__ ((visibility("default")))
 #else
 #define MEIPURU_EXPORT
-#endif
+#endif // Compiler
+#endif // MEIPURU_LIB
 
 #endif //MEIPURU_MEIPURUEXPORT_H
