926d9c6552d63e5e4159dc7aca0dd948eb5cbdb1
realth000 <a598919335@gmail.com>
2023-01-02 19:40:27 +0800

    fix(binding): Fix wrong memory usage
    Fix wrong memory usage.

--- binding/binding.cpp
+++ binding/binding.cpp
@@ -1,22 +1,24 @@
-#include "../include/binding.h"
+#include <cstring>
+
 #include "../include/MeipuruReader.h"
+#include "../include/binding.h"
 
 MeipuruTag *MeipuruReadTag(const char *filePath) {
     auto reader = Meipuru::MeipuruReader();
     auto tag = reader.readTagFromFile(filePath);
 
     auto *meipuruTag = new MeipuruTag{
-            tag->filePath.c_str(),
-            tag->fileName.c_str(),
-            tag->title.c_str(),
-            tag->artist.c_str(),
-            tag->albumTitle.c_str(),
-            tag->albumArtist.c_str(),
+            strdup(tag->filePath.c_str()),
+            strdup(tag->fileName.c_str()),
+            strdup(tag->title.c_str()),
+            strdup(tag->artist.c_str()),
+            strdup(tag->albumTitle.c_str()),
+            strdup(tag->albumArtist.c_str()),
             tag->year,
             tag->track,
             tag->albumTotalTrack,
-            tag->genre.c_str(),
-            tag->comment.c_str(),
+            strdup(tag->genre.c_str()),
+            strdup(tag->comment.c_str()),
             tag->bitRate,
             tag->sampleRate,
             tag->channels,
@@ -35,22 +37,22 @@ MeipuruID3v2Tag *MeipuruReadID3v2Tag(const char *filePath) {
     }
 
     auto *meipuruID3v2Tag = new MeipuruID3v2Tag{
-            id3v2Tag->filePath.c_str(),
-            id3v2Tag->fileName.c_str(),
-            id3v2Tag->title.c_str(),
-            id3v2Tag->artist.c_str(),
-            id3v2Tag->albumTitle.c_str(),
-            id3v2Tag->albumArtist.c_str(),
+            strdup(id3v2Tag->filePath.c_str()),
+            strdup(id3v2Tag->fileName.c_str()),
+            strdup(id3v2Tag->title.c_str()),
+            strdup(id3v2Tag->artist.c_str()),
+            strdup(id3v2Tag->albumTitle.c_str()),
+            strdup(id3v2Tag->albumArtist.c_str()),
             id3v2Tag->year,
             id3v2Tag->track,
             id3v2Tag->albumTotalTrack,
-            id3v2Tag->genre.c_str(),
-            id3v2Tag->comment.c_str(),
+            strdup(id3v2Tag->genre.c_str()),
+            strdup(id3v2Tag->comment.c_str()),
             id3v2Tag->bitRate,
             id3v2Tag->sampleRate,
             id3v2Tag->channels,
             id3v2Tag->length,
-            id3v2Tag->lyrics.c_str(),
+            strdup(id3v2Tag->lyrics.c_str()),
             (unsigned long) (id3v2Tag->lyrics.size()),
             id3v2Tag->albumCover.data,
             id3v2Tag->albumCover.size,
@@ -63,6 +65,15 @@ void MeipuruFreeTag(MeipuruTag *tag) {
     if (tag == nullptr) {
         return;
     }
+    // No custom destructor due to used by dart ffi, free manually.
+    free(tag->filePath);
+    free(tag->fileName);
+    free(tag->title);
+    free(tag->artist);
+    free(tag->albumTitle);
+    free(tag->albumArtist);
+    free(tag->genre);
+    free(tag->comment);
     delete tag;
     tag = nullptr;
 }
@@ -71,6 +82,16 @@ void MeipuruFreeID3v2Tag(MeipuruID3v2Tag *id3V2Tag) {
     if (id3V2Tag == nullptr) {
         return;
     }
+    // No custom destructor due to used by dart ffi, free manually.
+    free(id3V2Tag->filePath);
+    free(id3V2Tag->fileName);
+    free(id3V2Tag->title);
+    free(id3V2Tag->artist);
+    free(id3V2Tag->albumTitle);
+    free(id3V2Tag->albumArtist);
+    free(id3V2Tag->genre);
+    free(id3V2Tag->comment);
+    free(id3V2Tag->lyrics);
     delete id3V2Tag;
     id3V2Tag = nullptr;
 }
